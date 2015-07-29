6fe673a7d515cbf5181ad77412332e92d1dddf0c
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-07-29 17:21:02 -0400

    Add Inter and Diff

--- set.go
+++ set.go
@@ -21,17 +21,16 @@ func (s Int64Set) Len() int           { return len(s) }
 func (s Int64Set) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s Int64Set) Less(i, j int) bool { return s[i] < s[j] }
 
-func (s Int64Set) Union(t Int64Set) Int64Set {
+func (s Int64Set) Do(op set.Op, t Int64Set) Int64Set {
 	data := append(s, t...)
-	n := set.Union(data, len(s))
+	n := op(data, len(s))
 	return data[:n]
 }
 
-func (s Int64Set) Add(v ...int64) Int64Set {
-	data := append(s, v...)
-	n := set.Union(data, len(s))
-	return data[:n]
-}
+func (s Int64Set) Union(t Int64Set) Int64Set { return s.Do(set.Union, t) }
+func (s Int64Set) Inter(t Int64Set) Int64Set { return s.Do(set.Inter, t) }
+func (s Int64Set) Diff(t Int64Set) Int64Set  { return s.Do(set.Diff, t) }
+func (s Int64Set) Add(v ...int64) Int64Set   { return s.Do(set.Union, append(Int64Set{}, v...)) }
 
 // String set
 type StringSet []string
@@ -44,14 +43,13 @@ func NewStringSet(v ...string) StringSet {
 	return s
 }
 
-func (s StringSet) Union(t StringSet) StringSet {
+func (s StringSet) Do(op set.Op, t StringSet) StringSet {
 	data := append(s, t...)
-	n := set.Union(sort.StringSlice(data), len(s))
+	n := op(sort.StringSlice(data), len(s))
 	return data[:n]
 }
 
-func (s StringSet) Add(v ...string) StringSet {
-	data := append(s, v...)
-	n := set.Union(sort.StringSlice(data), len(s))
-	return data[:n]
-}
+func (s StringSet) Union(t StringSet) StringSet { return s.Do(set.Union, t) }
+func (s StringSet) Inter(t StringSet) StringSet { return s.Do(set.Inter, t) }
+func (s StringSet) Diff(t StringSet) StringSet  { return s.Do(set.Diff, t) }
+func (s StringSet) Add(v ...string) StringSet   { return s.Do(set.Union, append(StringSet{}, v...)) }
