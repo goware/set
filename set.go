21c52a7a7f7a79a82cfc158219e38f6ac56ba07b
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-09-23 11:10:13 -0400

    New Remove() method for sets

--- set.go
+++ set.go
@@ -27,10 +27,11 @@ func (s Int64Set) Do(op set.Op, t Int64Set) Int64Set {
 	return data[:n]
 }
 
-func (s Int64Set) Union(t Int64Set) Int64Set { return s.Do(set.Union, t) }
-func (s Int64Set) Inter(t Int64Set) Int64Set { return s.Do(set.Inter, t) }
-func (s Int64Set) Diff(t Int64Set) Int64Set  { return s.Do(set.Diff, t) }
-func (s Int64Set) Add(v ...int64) Int64Set   { return s.Do(set.Union, append(Int64Set{}, v...)) }
+func (s Int64Set) Union(t Int64Set) Int64Set  { return s.Do(set.Union, t) }
+func (s Int64Set) Inter(t Int64Set) Int64Set  { return s.Do(set.Inter, t) }
+func (s Int64Set) Diff(t Int64Set) Int64Set   { return s.Do(set.Diff, t) }
+func (s Int64Set) Add(v ...int64) Int64Set    { return s.Do(set.Union, append(Int64Set{}, v...)) }
+func (s Int64Set) Remove(v ...int64) Int64Set { return s.Do(set.Diff, NewInt64Set(v...)) }
 
 // String set
 type StringSet []string
@@ -49,7 +50,8 @@ func (s StringSet) Do(op set.Op, t StringSet) StringSet {
 	return data[:n]
 }
 
-func (s StringSet) Union(t StringSet) StringSet { return s.Do(set.Union, t) }
-func (s StringSet) Inter(t StringSet) StringSet { return s.Do(set.Inter, t) }
-func (s StringSet) Diff(t StringSet) StringSet  { return s.Do(set.Diff, t) }
-func (s StringSet) Add(v ...string) StringSet   { return s.Do(set.Union, append(StringSet{}, v...)) }
+func (s StringSet) Union(t StringSet) StringSet  { return s.Do(set.Union, t) }
+func (s StringSet) Inter(t StringSet) StringSet  { return s.Do(set.Inter, t) }
+func (s StringSet) Diff(t StringSet) StringSet   { return s.Do(set.Diff, t) }
+func (s StringSet) Add(v ...string) StringSet    { return s.Do(set.Union, append(StringSet{}, v...)) }
+func (s StringSet) Remove(v ...string) StringSet { return s.Do(set.Diff, NewStringSet(v...)) }
--- set_test.go
+++ set_test.go
@@ -9,26 +9,40 @@ import (
 
 func TestInt64Set(t *testing.T) {
 	s := set.NewInt64Set(1, 2, 3, 3)
-	s = s.Add(5, 5, 3, 2, 1, 4, 4, 5)
 
+	s = s.Add(5, 5, 3, 2, 1, 4, 4, 5)
 	if len(s) != 5 {
 		t.Error("expecting 5 values in the set")
 	}
-
 	if !reflect.DeepEqual([]int64(s), []int64{1, 2, 3, 4, 5}) {
+		t.Error("invalid set")
+	}
+
+	s = s.Remove(5, 4)
+	if len(s) != 3 {
+		t.Error("expecting 3 values in the set")
+	}
+	if !reflect.DeepEqual([]int64(s), []int64{1, 2, 3}) {
 		t.Error("invalid set")
 	}
 }
 
 func TestStringSet(t *testing.T) {
 	s := set.NewStringSet("a", "b", "a", "c")
-	s = s.Add("d", "a", "b", "c", "e")
 
+	s = s.Add("d", "a", "b", "c", "e")
 	if len(s) != 5 {
 		t.Error("expecting 5 values in the set")
 	}
-
 	if !reflect.DeepEqual([]string(s), []string{"a", "b", "c", "d", "e"}) {
+		t.Error("invalid set")
+	}
+
+	s = s.Remove("d", "a")
+	if len(s) != 3 {
+		t.Error("expecting 3 values in the set")
+	}
+	if !reflect.DeepEqual([]string(s), []string{"b", "c", "e"}) {
 		t.Error("invalid set")
 	}
 }
