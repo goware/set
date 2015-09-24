0bce6b70e913cca38296423f38694717938e8387
Peter Kieltyka <peter.kieltyka@pressly.com>
2015-09-24 16:16:33 -0400

    Sort set after operations

--- set.go
+++ set.go
@@ -24,13 +24,15 @@ func (s Int64Set) Less(i, j int) bool { return s[i] < s[j] }
 func (s Int64Set) Do(op set.Op, t Int64Set) Int64Set {
 	data := append(s, t...)
 	n := op(data, len(s))
-	return data[:n]
+	ss := data[:n]
+	sort.Sort(s)
+	return ss
 }
 
 func (s Int64Set) Union(t Int64Set) Int64Set  { return s.Do(set.Union, t) }
 func (s Int64Set) Inter(t Int64Set) Int64Set  { return s.Do(set.Inter, t) }
 func (s Int64Set) Diff(t Int64Set) Int64Set   { return s.Do(set.Diff, t) }
-func (s Int64Set) Add(v ...int64) Int64Set    { return s.Union(append(Int64Set{}, v...)) }
+func (s Int64Set) Add(v ...int64) Int64Set    { return s.Union(Int64Set(v)) }
 func (s Int64Set) Remove(v ...int64) Int64Set { return s.Diff(NewInt64Set(v...)) }
 func (s Int64Set) Exists(v int64) bool        { return len(s.Inter(Int64Set{v})) != 0 }
 
@@ -54,6 +56,6 @@ func (s StringSet) Do(op set.Op, t StringSet) StringSet {
 func (s StringSet) Union(t StringSet) StringSet  { return s.Do(set.Union, t) }
 func (s StringSet) Inter(t StringSet) StringSet  { return s.Do(set.Inter, t) }
 func (s StringSet) Diff(t StringSet) StringSet   { return s.Do(set.Diff, t) }
-func (s StringSet) Add(v ...string) StringSet    { return s.Union(append(StringSet{}, v...)) }
+func (s StringSet) Add(v ...string) StringSet    { return s.Union(StringSet(v)) }
 func (s StringSet) Remove(v ...string) StringSet { return s.Diff(NewStringSet(v...)) }
 func (s StringSet) Exists(v string) bool         { return len(s.Inter(StringSet{v})) != 0 }
